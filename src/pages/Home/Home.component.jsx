e0c6d469dfa19871c112eaffc22400e8b5306a4e
Riz <joshua.riz.layton@gmail.com>
2020-11-25 19:30:02 +0000

    MASTER: Correcting files

---
+++ src/pages/Home/Home.component.jsx
@@ -0,0 +1,14 @@
+// Import: Dependencies
+import React from "react";
+
+// Import: Elements
+import { Container } from "./Home.elements";
+
+// Page: Home
+export default function Home() {
+  return (
+    <Container>
+      <h1>Home</h1>
+    </Container>
+  );
+}
